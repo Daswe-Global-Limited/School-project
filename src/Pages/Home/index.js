fd6cc2bc325196897866daa96900a31c5fc0d148
olakved <olakved@gmail.com>
2022-12-13 13:10:19 +0100

    index update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomeSlider from '../../Components/HomeSlider'
+import ValuesComponent from '../../Components/Values'
+import SchoolsView from '../../Components/SchoolsView'
 
 function HomePage() {
   return (
     <div>
       <HomeSlider />
+      <ValuesComponent />
+      <SchoolsView />
     </div>
   )
 }
